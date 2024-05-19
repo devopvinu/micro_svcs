b7d58db0bda1fe4b774568b92442376eb8ad6fa4
devopvinu <atchivinod1998@gmail.com>
2024-05-19 14:58:44 +0530

    ip-changes

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://18.117.81.227:9999/'+e.target.name;
+    const url = 'http://18.233.161.40:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
